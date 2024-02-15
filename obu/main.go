7c379bbb52207e239d00c4baafcf96e73829c9bb
Josh Stockdale <joshdstockdale@gmail.com>
2024-02-15 11:24:33 -0500

    simple setup for websockets

---
+++ obu/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/joshdstockdale/go-truck-tracker/types"
+)
+
+const wsEndpoint = "ws://127.0.0.1:30000/ws"
+
+var sendInterval = time.Second
+
+func genLatLong() (float64, float64) {
+	return genCoord(), genCoord()
+}
+
+func genCoord() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func generateOBUIDs(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func main() {
+	obuIDS := generateOBUIDs(20)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		for i := 0; i < len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := types.OBUData{
+				OBUID: obuIDS[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Sent data %+v\n", data)
+
+		}
+		time.Sleep(sendInterval)
+	}
+}
